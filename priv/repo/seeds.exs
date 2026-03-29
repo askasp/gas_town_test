7a5b2834bd7a07d6f2c10c54d1babba4f858eb72
obsidian <aksel@stadler.no>
2026-03-30 00:54:48 +0200

    chore: format seeds.exs (gtt-8kg)

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -36,16 +36,47 @@ Seeds.insert_component!(
     type: "microcontroller",
     description: "8-bit AVR microcontroller with 8KB flash, 512B SRAM, 512B EEPROM",
     package_type: "DIP-8",
-    datasheet_url: "https://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-2586-AVR-8-bit-Microcontroller-ATtiny25-ATtiny45-ATtiny85_Datasheet.pdf"
+    datasheet_url:
+      "https://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-2586-AVR-8-bit-Microcontroller-ATtiny25-ATtiny45-ATtiny85_Datasheet.pdf"
   },
   [
-    %{name: "PB5/RESET/ADC0/dW", number: 1, type: "digital_io", voltage_min: 0.0, voltage_max: 5.5},
-    %{name: "PB3/ADC3/XTAL1/OC1B", number: 2, type: "analog_in", voltage_min: 0.0, voltage_max: 5.5},
-    %{name: "PB4/ADC2/XTAL2/OC1B", number: 3, type: "analog_in", voltage_min: 0.0, voltage_max: 5.5},
+    %{
+      name: "PB5/RESET/ADC0/dW",
+      number: 1,
+      type: "digital_io",
+      voltage_min: 0.0,
+      voltage_max: 5.5
+    },
+    %{
+      name: "PB3/ADC3/XTAL1/OC1B",
+      number: 2,
+      type: "analog_in",
+      voltage_min: 0.0,
+      voltage_max: 5.5
+    },
+    %{
+      name: "PB4/ADC2/XTAL2/OC1B",
+      number: 3,
+      type: "analog_in",
+      voltage_min: 0.0,
+      voltage_max: 5.5
+    },
     %{name: "GND", number: 4, type: "ground", voltage_min: 0.0, voltage_max: 0.0},
     %{name: "PB0/MOSI/SDA/AIN0/OC0A", number: 5, type: "pwm", voltage_min: 0.0, voltage_max: 5.5},
-    %{name: "PB1/MISO/AIN1/OC0B/OC1A", number: 6, type: "pwm", voltage_min: 0.0, voltage_max: 5.5},
-    %{name: "PB2/SCK/SCL/ADC1/T0", number: 7, type: "analog_in", voltage_min: 0.0, voltage_max: 5.5},
+    %{
+      name: "PB1/MISO/AIN1/OC0B/OC1A",
+      number: 6,
+      type: "pwm",
+      voltage_min: 0.0,
+      voltage_max: 5.5
+    },
+    %{
+      name: "PB2/SCK/SCL/ADC1/T0",
+      number: 7,
+      type: "analog_in",
+      voltage_min: 0.0,
+      voltage_max: 5.5
+    },
     %{name: "VCC", number: 8, type: "power", voltage_min: 2.7, voltage_max: 5.5}
   ],
   [
